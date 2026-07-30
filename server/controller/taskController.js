061cdb7f894eac4ef92023bded4a32bbcbe5cbca
atm-sakayet-hossain <mdshakhawat434@gmail.com>
2026-07-30 13:50:24 +0600

    update

--- server/controller/taskController.js
+++ server/controller/taskController.js
@@ -34,9 +34,9 @@ const updateTask = async (req, res) => {
         const {name, email, phone} = req.body
         const task = await taskSchema.findById(id)
         if(!task) return responceHandler.error(res, 404, "task not find")
-        if(name) return responceHandler.error(res, 400, "Name is Required")
-        if(email) return responceHandler.error(res, 400, "email is Required")
-        if(phone) return responceHandler.error(res, 400, "phone is Required")
+        if(name) task.name = name
+        if(email) task.email = email
+        if(phone) task.phone = phone
         await task.save()
         responceHandler.success(res, 200, "task updated")
     } catch (error) {
--- server/routes/taskRoute.js
+++ server/routes/taskRoute.js
@@ -5,6 +5,6 @@ const route = express.Router()
 
 route.post("/create", createTask );
 route.get("/get", getAllTask );
-route.put("/update:id", updateTask );
+route.put("/update/:id", updateTask );
 
 module.exports = route
